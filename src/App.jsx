99b3562cf59bac2605e7492b934ede4df19db806
Sebastian Sanchez <sesanchezra@unal.edu.co>
2022-08-20 14:15:56 -0500

    Css Root definition

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  // Design Link: https://dribbble.com/shots/4647242-TNQ-Dashboard-Members-Page
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
